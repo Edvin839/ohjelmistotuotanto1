94a147637ffe5c74e2444f465006caadbb505da4
Olli Rautiainen <74360204+ollirtnn@users.noreply.github.com>
2021-04-12 18:04:43 +0300

    Testaan vaan pelaako yhteydet.

--- Ohjelmistotuotanto1/Form1.Designer.cs
+++ Ohjelmistotuotanto1/Form1.Designer.cs
@@ -29,13 +29,25 @@ namespace Ohjelmistotuotanto1
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnElaPaina = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnElaPaina
+            // 
+            this.btnElaPaina.Location = new System.Drawing.Point(371, 145);
+            this.btnElaPaina.Name = "btnElaPaina";
+            this.btnElaPaina.Size = new System.Drawing.Size(159, 68);
+            this.btnElaPaina.TabIndex = 0;
+            this.btnElaPaina.Text = "Testinappi: Paina tästä";
+            this.btnElaPaina.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 414);
+            this.ClientSize = new System.Drawing.Size(996, 637);
+            this.Controls.Add(this.btnElaPaina);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@ namespace Ohjelmistotuotanto1
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnElaPaina;
     }
 }
 
